687ea54d03482f432c13978b360e7d08c21c8d91
notKamui <teillard.jimmy@outlook.fr>
2024-04-12 15:53:36 +0200

    Add inventory menu functionality

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ const SCREEN_HEIGHT: i32 = 50;
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 43;
 
+const INVENTORY_WIDTH: i32 = 50;
+
 const PANEL_HEIGHT: i32 = 5;
 const PANEL_Y: i32 = SCREEN_HEIGHT - PANEL_HEIGHT;
 const BAR_WIDTH: i32 = 20;
@@ -725,8 +727,83 @@ fn handle_keys(tcod: &mut Tcod, game: &mut Game, objects: &mut Vec<Object>) -> P
             }
             DidntTakeTurn
         }
+        (
+            Key {
+                code: KeyCode::Text,
+                ..
+            },
+            "i",
+            true,
+        ) => {
+            inventory_menu(
+                &game.inventory,
+                "Press the key next to an item to use it, or any other to cancel.\n",
+                &mut tcod.root,
+            );
+            TookTurn
+        }
         _ => DidntTakeTurn,
     }
+}
+
+fn menu<T: AsRef<str>>(header: &str, options: &[T], width: i32, root: &mut Root) -> Option<usize> {
+    assert!(
+        options.len() <= 26,
+        "Cannot have a menu with more than 26 options."
+    );
+
+    let header_height = root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header);
+    let height = options.len() as i32 + header_height;
+
+    let mut window = Offscreen::new(width, height);
+    window.set_default_foreground(colors::WHITE);
+    window.print_rect_ex(
+        0,
+        0,
+        width,
+        height,
+        BackgroundFlag::None,
+        TextAlignment::Left,
+        header,
+    );
+
+    for (index, option_text) in options.iter().enumerate() {
+        let menu_letter = (b'a' + index as u8) as char;
+        let text = format!("({}) {}", menu_letter, option_text.as_ref());
+        window.print_ex(
+            0,
+            header_height + index as i32,
+            BackgroundFlag::None,
+            TextAlignment::Left,
+            text,
+        );
+    }
+
+    let x = SCREEN_WIDTH / 2 - width / 2;
+    let y = SCREEN_HEIGHT / 2 - height / 2;
+    blit(&window, (0, 0), (width, height), root, (x, y), 1.0, 0.7);
+
+    root.flush();
+    let key = root.wait_for_keypress(true);
+
+    if key.printable.is_alphabetic() {
+        let index = key.printable.to_ascii_lowercase() as usize - 'a' as usize;
+        if index < options.len() {
+            Some(index)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+fn inventory_menu(inventory: &[Object], header: &str, root: &mut Root) -> Option<usize> {
+    let options = inventory
+        .iter()
+        .map(|item| item.name.clone())
+        .collect::<Vec<_>>();
+    menu(header, &options, INVENTORY_WIDTH, root)
 }
 
 fn main() {
